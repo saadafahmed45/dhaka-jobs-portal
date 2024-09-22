9d068901923291050c945ef72360047440c343b0
saadafahmed45 <hafijnahid45@gmail.com>
2024-09-23 01:14:21 +0600

    added the fade effect

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,11 +1,16 @@
-import React from "react";
-
+"use client";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"; // Import the AOS styles
 const Hero = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 }); // Initialize AOS with duration
+  }, []);
   return (
     <div className="">
       <div className="container md:px-24 px-8 py-24 mx-auto">
         <div className="items-center lg:flex">
-          <div className="w-full lg:w-1/2">
+          <div className="w-full lg:w-1/2" data-aos="fade-up">
             <div className="lg:max-w-lg">
               <h1 className="text-5xl font-bold text-gray-800  lg:text-5xl">
                 Best place to choose <br /> your{" "}
@@ -24,7 +29,10 @@ const Hero = () => {
             </div>
           </div>
 
-          <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+          <div
+            data-aos="fade-left"
+            className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2"
+          >
             <img
               className="w-full h-full lg:max-w-3xl"
               src="hero_image.svg"
